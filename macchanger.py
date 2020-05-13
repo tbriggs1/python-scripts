ce53d9c40566007f42d4ab906517fa11b5632a12
tbriggs1 <tombriggs93@protonmail.com>
2020-05-13 15:24:44 +0100

    second commit

--- macchanger.py
+++ macchanger.py
@@ -2,7 +2,11 @@
 
 import subprocess
 
-subprocess.call("sudo ifconfig wlan0 down", shell=True)
-subprocess.call("sudo macchanger -r wlan0", shell=True)
-subprocess.call("sudo ifconfig wlan0 up", shell=True)
+
+interface = (input("Please enter your Wireless interface card (e.g. wlan0): "))
+mac_address = (input("Please enter a MAC Address (e.g. \"00:11:22:33:44:55\") : "))
+
+subprocess.call("sudo ifconfig " + interface + " down", shell=True)
+subprocess.call("sudo macchanger -m " + mac_address + " " + interface , shell=True)
+subprocess.call("sudo ifconfig " + interface + " up", shell=True)
 subprocess.call("sudo ifconfig | grep ether", shell=True)
