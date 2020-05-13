f1f9b2ab7a18c354e5dcfa838d6322860fd76f8a
tbriggs1 <tombriggs93@protonmail.com>
2020-05-13 19:25:39 +0100

    if condition added

--- macchanger.py
+++ macchanger.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python 
 
 import subprocess
+import optparse
+
+def change_mac(interface, macAddress):
+    
+    subprocess.call([ "sudo", "ifconfig", interface, "down"])
+    subprocess.call(["sudo", "macchanger", "-m", macAddress, interface])
+    subprocess.call(["sudo", "ifconfig", interface, "up"])
+def get_arguements():
+
+    parser = optparse.OptionParser() 
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+    (options, arguements) = parser.parse_args()
+    if not options.interface:
+        parser.error("Please enter an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("Please enter a MacAddress, use --help for more info")
+    else:
+        return options
 
 
-interface = (input("Please enter your Wireless interface card (e.g. wlan0): "))
-mac_address = (input("Please enter a MAC Address (e.g. \"00:11:22:33:44:55\") : "))
 
-subprocess.call("sudo ifconfig " + interface + " down", shell=True)
-subprocess.call("sudo macchanger -m " + mac_address + " " + interface , shell=True)
-subprocess.call("sudo ifconfig " + interface + " up", shell=True)
-subprocess.call("sudo ifconfig | grep ether", shell=True)
+options = get_arguements()
+change_mac(options.interface, options.new_mac)
+
