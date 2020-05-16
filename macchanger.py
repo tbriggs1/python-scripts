adfd1c47fa8190219349571fd142683cc6c3a6c0
tbriggs1 <tombriggs93@protonmail.com>
2020-05-16 19:27:41 +0100

    latest

--- macchanger.py
+++ macchanger.py
@@ -2,14 +2,17 @@
 
 import subprocess
 import optparse
+import re
 
+def main():
+    options = get_arguements()
+    change_mac(options.interface, options.new_mac)
+    return_mac_address(options.interface)
 def change_mac(interface, macAddress):
-    
     subprocess.call([ "sudo", "ifconfig", interface, "down"])
-    subprocess.call(["sudo", "macchanger", "-m", macAddress, interface])
+    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", macAddress,])
     subprocess.call(["sudo", "ifconfig", interface, "up"])
 def get_arguements():
-
     parser = optparse.OptionParser() 
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
@@ -20,9 +23,13 @@ def get_arguements():
         parser.error("Please enter a MacAddress, use --help for more info")
     else:
         return options
+def return_mac_address(interface):
+    macresult = subprocess.check_output(["ifconfig", interface])
+    print_new_mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", macresult)
 
+    if print_new_mac_result:
+        print("Your new MAC Address is " + print_new_mac_result.group(0))
+    else:
+        print("Error: Could not see MAC Address, did you select the correct interface?")
 
-
-options = get_arguements()
-change_mac(options.interface, options.new_mac)
-
+main()    
--- monitormode.py
+++ monitormode.py
@@ -2,8 +2,25 @@
 
 import subprocess
 
-subprocess.call("sudo ifconfig wlan0 down", shell=True)
-subprocess.call("sudo iwconfig wlan0 mode monitor", shell=True)
-subprocess.call("sudo ifconfig wlan0 up", shell=True)
-subprocess.call("sudo iwconfig | grep Mode", shell=True)
+choice = input("Would you like to put your card into monitor mode or managed mode? - ")
+interface = input("Please enter your wireless interface card - ")
+def monitor_mode():
+    
+    subprocess.call("sudo ifconfig " + interface + " down", shell=True)
+    subprocess.call("sudo iwconfig " + interface + " mode monitor", shell=True)
+    subprocess.call("sudo ifconfig " + interface + " up", shell=True)
+    subprocess.call("sudo iwconfig | grep Mode", shell=True)
 
+def managed_mode():
+
+    subprocess.call("sudo ifconfig " + interface + " down", shell=True)
+    subprocess.call("sudo iwconfig " + interface + " mode managed", shell=True)
+    subprocess.call("sudo ifconfig " + interface + " up", shell=True)
+    subprocess.call("sudo iwconfig | grep Mode", shell=True)
+
+if choice == "managed":
+    managed_mode()
+elif choice == "monitor": 
+    monitor_mode()
+else:
+    print("Please enter managed or monitor")
